bdc429774674977916cf8e3a6256aed85bf32289
kimsia <kimcity@gmail.com>
2014-05-10 15:35:10 +0800

    initial commit

---
+++ load_data.R
@@ -0,0 +1,4 @@
+options(gsubfn.engine = "R")
+library(sqldf)
+query <- "SELECT * from file WHERE Date = '1/2/2007' OR Date = '2/2/2007'"
+dataset <- read.csv2.sql("./household_power_consumption.txt", sql=query)
