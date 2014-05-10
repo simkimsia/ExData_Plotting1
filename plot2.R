c23f99a49b7b425e3bf3b2adc138e2f06cd53b8f
kimsia <kimcity@gmail.com>
2014-05-11 01:21:38 +0800

    finished plot2.R and plot2.png

---
+++ plot2.R
@@ -0,0 +1,20 @@
+source("load_data.R")
+
+dataset <- load_data()
+
+days <- strptime(paste(dataset$Date, dataset$Time), format='%d/%m/%Y %H:%M:%S')
+
+if (Sys.info()["sysname"] == "Darwin") {
+  quartz() #for windows its windows() fpr unix it's X11()
+}
+
+png("plot2.png")
+
+# Create the bar plot
+plot(days, 
+     dataset$Global_active_power, 
+     type="l", 
+     xlab="", 
+     ylab="Global Active Power (kilowatts)")
+
+dev.off()
