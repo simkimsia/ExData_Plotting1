acce94be647e3fb867991ff3650122c8a4e1feeb
kimsia <kimcity@gmail.com>
2014-05-10 16:50:19 +0800

    finished plot1.R and plot1.png

--- load_data.R
+++ load_data.R
@@ -1,4 +1,7 @@
-options(gsubfn.engine = "R")
-library(sqldf)
-query <- "SELECT * from file WHERE Date = '1/2/2007' OR Date = '2/2/2007'"
-dataset <- read.csv2.sql("./household_power_consumption.txt", sql=query)
+load_data <- function () {
+  options(gsubfn.engine = "R")
+  library(sqldf)
+  query <- "SELECT * from file WHERE Date = '1/2/2007' OR Date = '2/2/2007'"
+  dataset <- read.csv2.sql("./household_power_consumption.txt", sql=query)
+  return (dataset)
+}
