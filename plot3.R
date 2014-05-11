85340adacedd132328ac760f87039f1076c058db
kimsia <kimcity@gmail.com>
2014-05-11 11:56:56 +0800

    added plot3.R and plot3.png

---
+++ plot3.R
@@ -0,0 +1,59 @@
+source("load_data.R")
+
+dataset <- load_data()
+
+days <- strptime(paste(dataset$Date, dataset$Time), format='%d/%m/%Y %H:%M:%S')
+
+y1 <- dataset$Sub_metering_1
+y2 <- dataset$Sub_metering_2
+y3 <- dataset$Sub_metering_3
+
+all_y <- c(y1, y2, y3)
+
+if (Sys.info()["sysname"] == "Darwin") {
+  quartz() #for windows its windows() fpr unix it's X11()
+}
+if (Sys.info()["sysname"] == "Windows") {
+  windows()
+}
+
+png("plot3.png")
+
+# Create the plot for y1
+plot(days, 
+     y1,
+     ylim=range(all_y),
+     type="l", 
+     xlab="", 
+     ylab="Energy sub metering", 
+     col="black"
+     )
+
+par(new = TRUE)
+# Create plot for y2
+plot(days, 
+     y2,
+     ylim=range(all_y),
+     type="l", 
+     xlab="", 
+     ylab="", 
+     axes = FALSE, 
+     col="red"
+     )
+
+par(new = TRUE)
+# Create plot for y3
+plot(days, 
+     y3,
+     ylim=range(all_y),
+     type="l", 
+     xlab="", 
+     ylab="", 
+     axes = FALSE,
+     col="blue"
+     )
+
+#create legend
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lty = 1) # optional legend
+
+dev.off()
